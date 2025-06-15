58e22ffb97326f9fcc76ce34514acc91a69a649d
bogdanzmeul <bogdanzmeul@gmail.com>
2025-06-15 22:47:33 +0300

    Improve buttons design

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleCartButton = document.getElementById('toggleCartButton');
     const cartAside = document.getElementById('cartAside');
 
-    // Перемикання видимості aside при натисканні на кнопку
     toggleCartButton.addEventListener('click', function() {
-        cartAside.classList.toggle('active'); // Додаємо/видаляємо клас 'active'
+        cartAside.classList.toggle('active');
         const isExpanded = cartAside.classList.contains('active');
-        toggleCartButton.setAttribute('aria-expanded', isExpanded); // Оновлюємо атрибут доступності
+        toggleCartButton.setAttribute('aria-expanded', isExpanded);
     });
 
-    // Додатково: Закриття aside при кліку поза ним
     document.addEventListener('click', function(event) {
-        // Перевіряємо, чи клік був поза aside та поза кнопкою toggleCartButton
         if (cartAside.classList.contains('active') &&
             !cartAside.contains(event.target) &&
             !toggleCartButton.contains(event.target)) {
